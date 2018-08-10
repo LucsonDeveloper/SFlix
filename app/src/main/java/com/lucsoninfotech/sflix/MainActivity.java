4d5e4c79c778be0cbae73dbb227a2fec933efe41
LucsonDeveloper <41437005+KhutalSandesh@users.noreply.github.com>
2018-08-10 16:33:07 +0530

    Done
    Done

---
+++ app/src/main/java/com/lucsoninfotech/sflix/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lucsoninfotech.sflix;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_facebook = findViewById(R.id.btn_facebook);
+        Button btn_gmail = findViewById(R.id.btn_gmail);
+        Button btn_twitter = findViewById(R.id.btn_twitter);
+        Button btn_flix = findViewById(R.id.btn_flix);
+        btn_facebook.setOnClickListener(this);
+        btn_gmail.setOnClickListener(this);
+        btn_twitter.setOnClickListener(this);
+        btn_flix.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_flix:
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.custom_mobile_login);
+                dialog.setTitle("Login Using" + R.string.app_name);
+                // set the custom dialog components - text, image and button
+                EditText mobile = dialog.findViewById(R.id.editTextMobile);
+                Button dialogButton = dialog.findViewById(R.id.buttonContinue);
+                dialog.setCancelable(false);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/lucsoninfotech/sflix/Mobile_Verification_Activity.java
@@ -0,0 +1,13 @@
+package com.lucsoninfotech.sflix;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Mobile_Verification_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_verification);
+    }
+}
